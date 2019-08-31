44b8ff9e0325ed70e73d3dc9b5bff7dc9e8398c3
Wojciech Kurcab <wojciech@kurcab.com>
2019-08-31 12:14:36 +0200

    31-08 update2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://fathomless-falls-64432.herokuapp.com/v1/task/';
+  const apiRoot = 'http://localhost:8084/v1/task/';
   const trelloApiRoot = 'https://fathomless-falls-64432.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
